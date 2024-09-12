6cc84c1ae29cb6ff0c5c96930170b30c4e60b111
spineda1208 <spineda1208@gmail.com>
2024-09-11 23:39:41 -0400

    update: with my information

--- write_ids.c
+++ write_ids.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    char GITHUB_ID[] = "Fill_in_Your_Github_Id";
-    char STUDENT_ID[] = "Fill_in_Your_Student_Id";
+    char GITHUB_ID[] = "spineda1208";
+    char STUDENT_ID[] = "sbp5827";
 
-    return 0 ;
+    return 0;
 }
